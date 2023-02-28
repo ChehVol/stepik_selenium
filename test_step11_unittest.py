59cc5d5a70f5e4d55d632a3c5864a05cd1ea9ae7
Voland <cheh.vol@yandex.ru>
2023-02-28 20:45:45 +0300

    added tests by unittest and pytest

---
+++ test_step11_unittest.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import unittest
+
+browser = webdriver.Chrome()
+
+
+def registration(link):
+    browser.get(link)
+    input1 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your first name"]')
+    input1.send_keys("Voland")
+    input2 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your last name"]')
+    input2.send_keys("Chehov")
+    input3 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your email"]')
+    input3.send_keys("@mail")
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+
+class TestRegistration(unittest.TestCase):
+    def test_reg1(self):
+        registration("http://suninjuly.github.io/registration1.html")
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!", 'Registratoin1 is failed')
+
+    def test_reg2(self):
+        registration("http://suninjuly.github.io/registration2.html")
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!", 'Registratoin2 is failed')
+
+
+if __name__ == "__main__":
+    unittest.main()
+
--- test_step13_pytest.py
+++ test_step13_pytest.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import pytest
 
-try: 
-    link = "http://suninjuly.github.io/registration1.html"
-    browser = webdriver.Chrome()
+browser = webdriver.Chrome()
+
+
+def registration(link):
     browser.get(link)
-
     input1 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your first name"]')
     input1.send_keys("Voland")
     input2 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your last name"]')
@@ -22,18 +23,22 @@ try:
     # ждем загрузки страницы
     time.sleep(1)
 
+
+def test_reg1():
+    registration("http://suninjuly.github.io/registration1.html")
     # находим элемент, содержащий текст
     welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
     # записываем в переменную welcome_text текст из элемента welcome_text_elt
     welcome_text = welcome_text_elt.text
-
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!" == welcome_text
-
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+    assert welcome_text == "Congratulations! You have successfully registered!", 'Registratoin1 is failed'
 
 
+def test_reg2():
+    registration("http://suninjuly.github.io/registration2.html")
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    welcome_text = welcome_text_elt.text
+    assert welcome_text == "Congratulations! You have successfully registered!", 'Registratoin2 is failed'
+
+
+if __name__ == "__main__":
+    pytest.main()
